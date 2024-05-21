fc328a9e0cd84631e3c4b60b34b03a72ab72979e
DinhLang <dinhlang86@gmail.com>
2024-05-21 21:15:01 +1000

    create product endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import auth_api, category_api, user_api
+from app.routers import auth_api, category_api, product_api, user_api
 
 app = FastAPI()
 
@@ -8,3 +8,4 @@ app = FastAPI()
 app.include_router(auth_api.router)
 app.include_router(user_api.router)
 app.include_router(category_api.router)
+app.include_router(product_api.router)
