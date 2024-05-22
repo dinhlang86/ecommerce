53e9c766f3a7939cb9390ec644f4e2256960e716
DinhLang <dinhlang86@gmail.com>
2024-05-22 20:58:42 +1000

    setup async pytest

--- test/conftest.py
+++ test/conftest.py
@@ -1,31 +1,26 @@
-import asyncio
-from typing import Any, AsyncGenerator, Generator
+from datetime import timedelta
+from typing import AsyncGenerator
 
-import pytest
 import pytest_asyncio
+from httpx import ASGITransport, AsyncClient
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.core.config import config
-from app.core.database import db_connection_str
+from app.core.database import db_connection_str, get_async_session
+from app.main import app
 from app.models.user import User
+from app.services.auth_service import create_access_token
 
 db_name = f"{config.db_name}_test"
 test_db_connection_str = f"postgresql+asyncpg://{config.db_username}:{config.db_password}@\
 {config.db_host}:{config.db_port}/{db_name}"
 
 
-@pytest.fixture(scope="session")
-def event_loop(request: Any) -> Generator[asyncio.AbstractEventLoop, None, None]:
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
 @pytest_asyncio.fixture(scope="session")
-async def test_db(event_loop: asyncio.AbstractEventLoop) -> AsyncGenerator[None, None]:
+async def test_engine() -> AsyncGenerator[AsyncEngine, None]:
     assert test_db_connection_str != db_connection_str
     engine: AsyncEngine = create_async_engine(
         f"""postgresql+asyncpg://postgres:{config.db_password}@{config.db_host}:{config.db_port}
@@ -35,29 +30,30 @@ async def test_db(event_loop: asyncio.AbstractEventLoop) -> AsyncGenerator[None,
     async with engine.connect() as conn:
         await conn.execute(text(f"DROP DATABASE IF EXISTS {db_name}"))
         await conn.execute(text(f"CREATE DATABASE {db_name}"))
-    yield
+    test_db_engine = create_async_engine(
+        test_db_connection_str,
+        isolation_level="AUTOCOMMIT",
+    )
+    yield test_db_engine
+    await test_db_engine.dispose()
     async with engine.connect() as conn:
         await conn.execute(text(f"DROP DATABASE IF EXISTS {db_name}"))
     await engine.dispose()
 
 
 @pytest_asyncio.fixture(scope="function")
-async def async_session(test_db: Any) -> AsyncGenerator[AsyncSession, None]:
-    engine = create_async_engine(
-        test_db_connection_str,
-        isolation_level="AUTOCOMMIT",
-    )
-    session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async def async_session(test_engine: AsyncEngine) -> AsyncGenerator[AsyncSession, None]:
+    session = async_sessionmaker(test_engine, expire_on_commit=False, class_=AsyncSession)
     async with session() as s:
-        async with engine.begin() as conn:
+        async with test_engine.begin() as conn:
             await conn.run_sync(SQLModel.metadata.create_all)
 
         yield s
 
-    async with engine.begin() as conn:
+    async with test_engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.drop_all)
 
-    await engine.dispose()
+    await test_engine.dispose()
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -66,9 +62,23 @@ async def admin_user(async_session: AsyncSession) -> AsyncGenerator[User, None]:
         id=1,
         email="admin@gmail.com",
         name="admin",
-        password="admin",
+        password="$2b$12$TsmFp5ID4jr0bYTF66zgOeqf0tW/Y4J7.Dves2GT4oL9ipV/0N8te",  # admin
         role="admin",
     )
     async_session.add(user)
     await async_session.commit()
     yield user
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client(async_session: AsyncSession) -> AsyncGenerator[AsyncClient, None]:
+    app.dependency_overrides[get_async_session] = lambda: async_session
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://localhost:8081"  # type: ignore
+    ) as c:
+        yield c
+
+
+@pytest_asyncio.fixture(scope="function")
+async def admin_token() -> str:
+    return create_access_token("admin@gmail.com", 1, "admin", timedelta(minutes=20))
--- test/test_user.py
+++ test/test_user.py
@@ -1,10 +1,33 @@
+import pytest
+from httpx import AsyncClient
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.models.user import User
 
 
+@pytest.mark.asyncio
 async def test_user_creation(admin_user: User, async_session: AsyncSession) -> None:
     admin: User | None = await async_session.get(User, admin_user.id)
     assert admin is not None
     assert admin.email == admin_user.email
     assert admin.name == admin_user.name
+
+
+@pytest.mark.asyncio
+async def test_get_all_users(admin_user: User, admin_token: str, client: AsyncClient) -> None:
+    # Test invalid token
+    # response = await client.get(
+    #     "/user",
+    #     headers={"Authorization": "Bearer dsafdfasdjl"},
+    # )
+    # assert response.status_code == 401
+    # Test valid token
+    response = await client.get(
+        "/user",
+        headers={"Authorization": f"Bearer {admin_token}"},
+    )
+    assert response.status_code == 200
+    users = response.json()
+    assert len(users) == 1
+    assert users[0]["email"] == admin_user.email
+    assert users[0]["name"] == admin_user.name
