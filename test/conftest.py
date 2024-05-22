a12fd4d028400ee79465ac547417c5c24118f81d
DinhLang <dinhlang86@gmail.com>
2024-05-22 20:14:34 +1000

    Create db, tables successfully

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,7 @@
-from typing import AsyncGenerator
+import asyncio
+from typing import Any, AsyncGenerator, Generator
 
+import pytest
 import pytest_asyncio
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
@@ -7,14 +9,24 @@ from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.core.config import config
+from app.core.database import db_connection_str
 from app.models.user import User
 
 db_name = f"{config.db_name}_test"
-db_connection_str = f"postgresql+asyncpg://{config.db_username}:{config.db_password}@\
+test_db_connection_str = f"postgresql+asyncpg://{config.db_username}:{config.db_password}@\
 {config.db_host}:{config.db_port}/{db_name}"
 
 
-async def prep_db() -> None:
+@pytest.fixture(scope="session")
+def event_loop(request: Any) -> Generator[asyncio.AbstractEventLoop, None, None]:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def test_db(event_loop: asyncio.AbstractEventLoop) -> AsyncGenerator[None, None]:
+    assert test_db_connection_str != db_connection_str
     engine: AsyncEngine = create_async_engine(
         f"""postgresql+asyncpg://postgres:{config.db_password}@{config.db_host}:{config.db_port}
             /postgres""",
@@ -23,13 +35,16 @@ async def prep_db() -> None:
     async with engine.connect() as conn:
         await conn.execute(text(f"DROP DATABASE IF EXISTS {db_name}"))
         await conn.execute(text(f"CREATE DATABASE {db_name}"))
+    yield
+    async with engine.connect() as conn:
+        await conn.execute(text(f"DROP DATABASE IF EXISTS {db_name}"))
+    await engine.dispose()
 
 
 @pytest_asyncio.fixture(scope="function")
-async def async_session() -> AsyncGenerator[AsyncSession, None]:
-    await prep_db()
+async def async_session(test_db: Any) -> AsyncGenerator[AsyncSession, None]:
     engine = create_async_engine(
-        db_connection_str,
+        test_db_connection_str,
         isolation_level="AUTOCOMMIT",
     )
     session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
--- test/test_user.py
+++ test/test_user.py
@@ -3,7 +3,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from app.models.user import User
 
 
-async def test_user(admin_user: User, async_session: AsyncSession) -> None:
+async def test_user_creation(admin_user: User, async_session: AsyncSession) -> None:
     admin: User | None = await async_session.get(User, admin_user.id)
     assert admin is not None
     assert admin.email == admin_user.email
